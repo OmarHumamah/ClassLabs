d2e18e805a96944a295ce2510b5b731887095aa1
OmarHumamah <omar.nabeel.h@gmail.com>
2021-06-20 15:10:20 +0300

    class01

---
+++ app.js
@@ -0,0 +1,14 @@
+let name = prompt('what is your name?');
+console.log(name)
+alert('Hallo' + '-' + name)
+let age = prompt('How old are you?')
+console.log(age)
+alert('So you are'+'('+ age +')'+ '. intresting!')
+let city = prompt('Which city do you like travle to'+'-'+name+'?')
+console.log(city)
+alert(city +'-'+ 'is very good choise.')
+let car = prompt("Do you have a car?")
+console.log(car)
+alert('Enjoy!')
+
+
